510fb8a3c9711be7311fa09c82d919638c5c9a60
Pavel Novikov <pavel_novikov1@epam.com>
2020-06-28 00:14:00 +0300

    add some secret creation, config map, telnet pod

--- root/run.ps1
+++ root/run.ps1
@@ -1,3 +1,6 @@
 oc process -f .\test-config-map.yaml -p TEST_PARAM=SuperValue | oc apply -f -
 oc get configmap test-config-map -o json
 oc process -f .\test-cron-job.yaml | oc apply -f -
+oc process -f .\test-config-map.yaml --param-file=test-config-map-params | oc apply -f -
+
+oc apply -f telnet-pod.yaml
